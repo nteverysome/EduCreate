0125459790bd5198cfc1710dfa0919b962b45ec8
nteverysome <128994229+nteverysome@users.noreply.github.com>
2025-06-26 18:58:58 +0800

    Fix import path for prisma in versions.ts - use correct relative path

--- pages/api/activities/[id]/versions.ts
+++ pages/api/activities/[id]/versions.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '../../auth/[...nextauth]';
-import prisma from '../../../lib/prisma';
+import prisma from '../../../../lib/prisma';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // 檢查用者是否已登入
