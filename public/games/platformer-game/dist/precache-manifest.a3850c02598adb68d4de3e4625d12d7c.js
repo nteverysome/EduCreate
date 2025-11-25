5a32f3f1f0010c12fff6d1c9ec16b8292a92f14e
nteverysome <128994229+nteverysome@users.noreply.github.com>
2025-11-25 14:01:58 +0800

    fix: add dist directories for dino-chrome-clone and platformer-game games

---
+++ public/games/platformer-game/dist/precache-manifest.a3850c02598adb68d4de3e4625d12d7c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "e365b3a805389d0c625d",
+    "url": "game.1b621642e05b43878c38.js"
+  },
+  {
+    "revision": "7e289b71f73d2e94ab7d",
+    "url": "vendors.85a1f98c7d62ff6befde.js"
+  },
+  {
+    "revision": "a5212f082f0f31625d2cbdbe6c830ead",
+    "url": "index.html"
+  },
+  {
+    "revision": "57040e5677322118f6d56a1d9e43c5c6",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "10376b717088f1da6efea303b63ceefe",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "2fd37c200e64a8b50e4908614ddec5e8",
+    "url": "sw.js"
+  },
+  {
+    "revision": "82035e76949b35a0e630343e914c6706",
+    "url": "assets/icons/icons-192.png"
+  },
+  {
+    "revision": "01018acda369257b41cc1c66d897e1fb",
+    "url": "assets/icons/icons-512.png"
+  },
+  {
+    "revision": "321451d7e72e30242d0298648c4a36b2",
+    "url": "assets/img/bee.png"
+  },
+  {
+    "revision": "171d49a74c8506688c68d953616de331",
+    "url": "assets/img/background.png"
+  },
+  {
+    "revision": "a1fcca8be8b6b14075f15568e6f6a1d1",
+    "url": "assets/img/coin.png"
+  },
+  {
+    "revision": "390e30cfd9d8352bc26fd0c07596c984",
+    "url": "assets/img/goal.png"
+  },
+  {
+    "revision": "0f80f7d1f852adfe06bb4fd850258912",
+    "url": "assets/img/controls.png"
+  },
+  {
+    "revision": "dc4ae2aa19d995edecc0f81c74300c4f",
+    "url": "assets/img/player.png"
+  },
+  {
+    "revision": "5242ead74cb23824ae5d0e2316c6c849",
+    "url": "assets/img/tile-left.png"
+  },
+  {
+    "revision": "671bbf5dcbdb8d922ab00bbd76746a18",
+    "url": "assets/img/tile-middle.png"
+  },
+  {
+    "revision": "a1d3f659688e62d4e9c0219c9f9885e4",
+    "url": "assets/img/tile-right.png"
+  },
+  {
+    "revision": "2e65cd566ce501c75e67c45e77952ec9",
+    "url": "assets/img/slime.png"
+  },
+  {
+    "revision": "80f2eeb1798f195a6f6a6f690c34b625",
+    "url": "assets/spine/boy.atlas"
+  },
+  {
+    "revision": "99c3f634326e4e27229423f199f5a3f9",
+    "url": "assets/img/tile-single.png"
+  },
+  {
+    "revision": "fb670c5cebe1e4340c76fea6d527fdc4",
+    "url": "assets/spine/boy.json"
+  },
+  {
+    "revision": "94452586b366f6514a918ee15281f2ec",
+    "url": "assets/spine/boy.png"
+  }
+];
---
+++ public/games/platformer-game/dist/sw.js
@@ -0,0 +1,4 @@
+importScripts("precache-manifest.a3850c02598adb68d4de3e4625d12d7c.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
