fb612d28a07477fd40870cc1c649782b6bf983e4
nteverysome <128994229+nteverysome@users.noreply.github.com>
2026-01-30 16:32:50 +0800

    fix: 修復 Blastemup 遊戲資源路徑問題 - 將相對路徑改為絕對路徑

--- public/games/blastemup-game/scenes/bootloader.js
+++ public/games/blastemup-game/scenes/bootloader.js
@@ -31,52 +31,52 @@ class Bootloader extends Phaser.Scene {
     Array(6)
       .fill(0)
       .forEach((_, i) => {
-        this.load.audio(`muzik${i}`, `assets/sounds/muzik${i}.mp3`);
+        this.load.audio(`muzik${i}`, `/games/blastemup-game/assets/sounds/muzik${i}.mp3`);
       });
 
     // 加載遊戲圖片
-    this.load.image("ship1_1", "assets/images/starship.png");
-    this.load.image("foeship", "assets/images/foeship.png");
-    this.load.image("pello", "assets/images/pello.png");
-    this.load.image("hex", "assets/images/hex64.png");
-    this.load.image("asteroid", "assets/images/asteroid.png");
+    this.load.image("ship1_1", "/games/blastemup-game/assets/images/starship.png");
+    this.load.image("foeship", "/games/blastemup-game/assets/images/foeship.png");
+    this.load.image("pello", "/games/blastemup-game/assets/images/pello.png");
+    this.load.image("hex", "/games/blastemup-game/assets/images/hex64.png");
+    this.load.image("asteroid", "/games/blastemup-game/assets/images/asteroid.png");
 
     // 加載音效
-    this.load.audio("splash", "assets/sounds/splash.mp3");
-    this.load.audio("game-over", "assets/sounds/game-over.mp3");
-    this.load.audio("explosion", "assets/sounds/explosion.mp3");
-    this.load.audio("shot", "assets/sounds/shot.mp3");
-    this.load.audio("foeshot", "assets/sounds/foeshot.mp3");
-    this.load.audio("pick", "assets/sounds/pick.mp3");
-    this.load.audio("asteroid", "assets/sounds/asteroid.mp3");
+    this.load.audio("splash", "/games/blastemup-game/assets/sounds/splash.mp3");
+    this.load.audio("game-over", "/games/blastemup-game/assets/sounds/game-over.mp3");
+    this.load.audio("explosion", "/games/blastemup-game/assets/sounds/explosion.mp3");
+    this.load.audio("shot", "/games/blastemup-game/assets/sounds/shot.mp3");
+    this.load.audio("foeshot", "/games/blastemup-game/assets/sounds/foeshot.mp3");
+    this.load.audio("pick", "/games/blastemup-game/assets/sounds/pick.mp3");
+    this.load.audio("asteroid", "/games/blastemup-game/assets/sounds/asteroid.mp3");
 
     // 加載字體
     this.load.bitmapFont(
       "arcade",
-      "assets/fonts/arcade.png",
-      "assets/fonts/arcade.xml"
+      "/games/blastemup-game/assets/fonts/arcade.png",
+      "/games/blastemup-game/assets/fonts/arcade.xml"
     );
     this.load.bitmapFont(
       "wendy",
-      "assets/fonts/arcade.png",
-      "assets/fonts/wendy.xml"
+      "/games/blastemup-game/assets/fonts/arcade.png",
+      "/games/blastemup-game/assets/fonts/wendy.xml"
     );
     this.load.bitmapFont(
       "starshipped",
-      "assets/fonts/starshipped.png",
-      "assets/fonts/starshipped.xml"
+      "/games/blastemup-game/assets/fonts/starshipped.png",
+      "/games/blastemup-game/assets/fonts/starshipped.xml"
     );
 
     // 加載精靈表
-    this.load.spritesheet("shot", "assets/images/shot.png", {
+    this.load.spritesheet("shot", "/games/blastemup-game/assets/images/shot.png", {
       frameWidth: 32,
       frameHeight: 32,
     });
-    this.load.spritesheet("shotfoe", "assets/images/shotfoe.png", {
+    this.load.spritesheet("shotfoe", "/games/blastemup-game/assets/images/shotfoe.png", {
       frameWidth: 32,
       frameHeight: 32,
     });
-    this.load.spritesheet("energy", "assets/images/energy.png", {
+    this.load.spritesheet("energy", "/games/blastemup-game/assets/images/energy.png", {
       frameWidth: 32,
       frameHeight: 32,
     });
